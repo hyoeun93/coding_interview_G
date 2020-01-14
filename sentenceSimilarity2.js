833a5af5a8b08ff33974519013b78d42e370ee61
HyoEun Lee <hyoeun93@gmail.com>
2020-01-13 23:04:54 -0500

    first commit

---
+++ sentenceSimilarity2.js
@@ -0,0 +1,6 @@
+//Given two sentences words1, words2 (each represented as an array of strings), 
+//and a list of similar word pairs pairs, determine of two sentences are similar.
+
+var areSentencesSimilarTwo = function(words1, words2, pairs) {
+    
+}
