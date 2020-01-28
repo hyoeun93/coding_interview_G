37f9498b8543165ffcd42bb191ecd521a4a00708
HyoEun Lee <hyoeun93@gmail.com>
2020-01-28 04:44:29 -0500

    reverseInteger solution

--- reverseInteger.js
+++ reverseInteger.js
@@ -1,5 +1,9 @@
 //Given a 32-bit signed integer, reverse digits of an integer
 
 var reverse = function(x) {
+    var reverseNum = parseInt(x.toString().split("").reverse().join(""));
     
+    if(reverseNum>(Math.pow(2,31)-1)) return 0;
+    
+    return x >= 0 ? reverseNum : 0 - reverseNum;
 }
